28a1804f0c698095fc2c869183e2d16ec27a5888
thiago-michelini <tigomm@gmail.com>
2017-05-05 22:52:58 -0400

    comitando

--- src/test/groovy/grails/rest/ProductControllerSpec.groovy
+++ src/test/groovy/grails/rest/ProductControllerSpec.groovy
@@ -15,6 +15,12 @@ import spock.lang.*
 class ProductControllerSpec extends HibernateSpec {
 
     def setup() {
+        Product.saveAll(
+                new Product(name: 'Maça', price: 3.8),
+                new Product(name: 'Laranja', price: 4.2),
+                new Product(name: 'Banana', price: 2.65),
+                new Product(name: 'Caki', price: 7.0)
+        )
     }
 
     def cleanup() {
@@ -24,8 +30,12 @@ class ProductControllerSpec extends HibernateSpec {
         jsonSmartViewResolver(JsonViewResolver)
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "teste se a acao search localiza resultados"() {
+        when: "eh executada uma consulta que localiza resultados"
+        controller.search('ran', 10)
+
+        then: "a resposta estah correta"
+        response.json.size() == 1
+        response.json[0].name == 'Laranja'
     }
 }
